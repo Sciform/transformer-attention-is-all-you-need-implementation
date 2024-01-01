4c609b2911040678fd9af64908fe2890a76482dc
unknown <ext.ursula.mayer@flaconi.de>
2024-01-01 23:16:46 +0100

    fix conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,8 +26,7 @@ extensions = [
     'sphinx.ext.viewcode',  # Add a link to the Python source code for classes, functions etc.
     'sphinx_autodoc_typehints', # Automatically document param types (less noise in class signature)
     'nbsphinx',  # Integrate Jupyter Notebooks and Sphinx
-    'IPython.sphinxext.ipython_console_highlighting',
-    'pytorch_sphinx_theme'
+    'IPython.sphinxext.ipython_console_highlighting'
 ]
 
 # Mappings for sphinx.ext.intersphinx. Projects have to have Sphinx-generated doc! (.inv file)
