23dd396c8fc10b34e077368e2bb21906d81d7aa5
unknown <ext.ursula.mayer@flaconi.de>
2024-01-04 14:15:45 +0100

    introduced git pull and bug fixes

--- mt_transformer/utils/tf_utils.py
+++ mt_transformer/utils/tf_utils.py
@@ -5,7 +5,7 @@ import torch
 
 def get_proc_device():
     
-    device = torch.device("cuda-gpu" if torch.cuda.is_available() else "cpu")
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     logging.info(f'Tf_utils: The processing unit is {device}')
     
     return device
