472c56419f7bb854ee59369249373a83aade1c49
Ursula Maria Mayer <ursula.maria.mayer@sciform.com>
2024-02-03 21:07:43 +0100

    add doc to main file

--- sci_tf/__init__.py
+++ sci_tf/__init__.py
@@ -1,3 +1,6 @@
 """
-documentation of src folder
+Documentation of the implementation of the
+original Transformer architecture as published 
+in the paper by Vaswani A. et al., "Attention is all you need", 2017, 
+https://arxiv.org/abs/1706.03762.
 """
